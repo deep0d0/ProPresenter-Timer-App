5f6d7e2c61adf18aaf63f0fcaa33fc30e758be5e
Deep Desai <60404672+deep0d0@users.noreply.github.com>
2024-04-15 13:42:48 -0400

    Initial Build

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 144F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 131);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(750, 122);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 144F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(110, 397);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(750, 122);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 144F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(110, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1196, 122);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Video Time Remaining...";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 144F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(110, 266);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(579, 122);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Audio Time Remaining...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1414, 497);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Pro Presenter Timer App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+
+            int height = this.Height / 4;
+
+            this.label3.Location = new Point(0, 0);
+            this.label3.Width = this.Width;
+            this.label3.Height = height;
+
+            this.label1.Location = new Point(0, height);
+            this.label1.Width = this.Width;
+            this.label1.Height = height;
+
+            this.label4.Location = new Point(0, height * 2);
+            this.label4.Width = this.Width;
+            this.label4.Height = height;
+
+            this.label2.Location = new Point(0, height * 3);
+            this.label2.Width = this.Width;
+            this.label2.Height = height;
+
+            this.label1.TextAlign = ContentAlignment.MiddleCenter;
+            this.label2.TextAlign = ContentAlignment.MiddleCenter;
+            this.label3.TextAlign = ContentAlignment.MiddleCenter;
+            this.label4.TextAlign = ContentAlignment.MiddleCenter;
+
+            this.label1.Text = GetVideoCountDown();
+            this.label2.Text = GetAudioTimeRemains();
+            this.timer1.Enabled = true;
+        }
+
+        private string GetAudioTimeRemains()
+        {
+            string totalAudioTime = GetAudioCountDown();
+            string currentAudioTime = GetAudioCountDown(true);
+            double timeRemains = double.Parse(totalAudioTime) - double.Parse(currentAudioTime);
+            TimeSpan timeRemainsSpan = TimeSpan.FromSeconds(timeRemains);
+            return timeRemainsSpan.ToString(@"hh\:mm\:ss");
+        }
+
+        private string GetVideoCountDown()
+        {
+            string CountDown = "00:00:00";
+            string uri = $"{ConfigurationManager.AppSettings["ProPresenterIPAddress"]}/v1/timer/video_countdown";
+
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(uri);
+            req.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                string result = sr.ReadToEnd();
+                CountDown = JsonConvert.DeserializeObject<string>(result);
+            }
+
+            return CountDown;
+        }
+
+        private string GetAudioCountDown(bool isCurrentTime = false)
+        {
+            string CountDown = "00:00:00";
+            string uri;
+            if (isCurrentTime) //Get Current Audio Time
+                uri = $"{ConfigurationManager.AppSettings["ProPresenterIPAddress"]}/v1/transport/audio/time";
+            else //Get Total Audio Time
+                uri = $"{ConfigurationManager.AppSettings["ProPresenterIPAddress"]}/v1/transport/audio/current";
+
+            HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(uri);
+            req.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader sr = new StreamReader(response.GetResponseStream());
+                string result = sr.ReadToEnd();
+
+                if (isCurrentTime)
+                    CountDown = JsonConvert.DeserializeObject<string>(result);
+                else
+                {
+                    AudioClass audioClass = JsonConvert.DeserializeObject<AudioClass>(result);
+                    CountDown = audioClass.duration;
+                }
+            }
+
+            return CountDown;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = GetVideoCountDown();
+            this.label2.Text = GetAudioTimeRemains();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            int height = this.Height / 4;
+
+            this.label3.Location = new Point(0, 0);
+            this.label3.Width = this.Width;
+            this.label3.Height = height;
+
+            this.label1.Location = new Point(0, height);
+            this.label1.Width = this.Width;
+            this.label1.Height = height;
+
+            this.label4.Location = new Point(0, height * 2);
+            this.label4.Width = this.Width;
+            this.label4.Height = height;
+
+            this.label2.Location = new Point(0, height * 3);
+            this.label2.Width = this.Width;
+            this.label2.Height = height;
+
+            this.label1.TextAlign = ContentAlignment.MiddleCenter;
+            this.label2.TextAlign = ContentAlignment.MiddleCenter;
+            this.label3.TextAlign = ContentAlignment.MiddleCenter;
+            this.label4.TextAlign = ContentAlignment.MiddleCenter;
+
+
+        }
+    }
+
+    public class AudioClass
+    {
+        public bool is_playing { get; set; }
+        public Dictionary<string, string> uuid { get; set; }
+        public string name { get; set; }
+        public string artist { get; set; }
+        public bool audio_only { get; set; }
+        public string duration { get; set; }
+    }
+}
